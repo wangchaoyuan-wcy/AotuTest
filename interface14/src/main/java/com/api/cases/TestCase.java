2ee2341ab670b2d3dcd73e714b887be1379be3a2
wangchaoyuan <15103361904@163.com>
2021-01-10 19:56:26 +0800

    添加用例

--- interface14/src/main/java/com/api/cases/TestCase.java
+++ interface14/src/main/java/com/api/cases/TestCase.java
@@ -8,6 +8,7 @@ import com.api.db.mysql.daos.DataDao;
 import com.api.utils.ConfigFile;
 import com.api.utils.Controller;
 import com.api.utils.Login;
+import com.google.common.eventbus.AllowConcurrentEvents;
 import org.springframework.context.ApplicationContext;
 import org.springframework.context.support.ClassPathXmlApplicationContext;
 import org.testng.Assert;
@@ -21,7 +22,7 @@ import static com.api.utils.InterfaceName.STUDYREPORT;
 import static org.hamcrest.MatcherAssert.assertThat;
 import static org.hamcrest.Matchers.is;
 
-
+@SuppressWarnings("all")
 public class TestCase {
     ApplicationContext context = new ClassPathXmlApplicationContext("bean.xml");
     Controller controller = (Controller) context.getBean("controller");
@@ -34,8 +35,8 @@ public class TestCase {
 
     }*/
 
-    @Test
-    public void test() throws IOException{
+    @Test(description = "学习报告正向用例")
+    public void LearningReport() throws IOException{
         DataDao dataDao=new DataDao();
         JSONObject jsonObject=JSON.parseObject(dataDao.selectTestDataListById("2").get(0).getRequest());
         JSONObject responseData=JSON.parseObject(dataDao.selectTestDataListById("2").get(0).getResponse());
@@ -46,6 +47,20 @@ public class TestCase {
         String msg = responseJson.getString("msg");
         String expectedMsg=responseData.getString("msg");
         //Assert.assertEquals(code,expectedValue,"正确");
+        assertThat("校验返回值code错误，验证code",code,is(expectedCode));
+        assertThat("校验返回值msg错误，验证msg",msg,is(expectedMsg));
+    }
+    @Test(description = "学习报告错误用例")
+    public void test2() throws IOException{
+        DataDao dataDao=new DataDao();
+        JSONObject jsonObject=JSON.parseObject(dataDao.selectTestDataListById("3").get(0).getRequest());
+        JSONObject responseData=JSON.parseObject(dataDao.selectTestDataListById("3").get(0).getResponse());
+        String response =controller.getHttpClient().Post(jsonObject, ConfigFile.getUrl(STUDYREPORT),"crmadmin");
+        JSONObject responseJson = JSONObject.parseObject(response);
+        String code =responseJson.getString("code");
+        String expectedCode=responseData.getString("code");
+        String msg = responseJson.getString("msg");
+        String expectedMsg=responseData.getString("msg");
         assertThat("校验返回值code错误，验证code",code,is(expectedCode));
         assertThat("校验返回值msg错误，验证msg",msg,is(expectedMsg));
     }
